d298b357cb84dd69ebd30ff4479a148d82b16aba
Ivan Jolic <ivan.jolic@metoda.com>
2019-11-14 11:19:37 +0100

    Add site title

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
+    <div class="site-title">Bugs of the City</div>
     <layout-composer
       :displayComponents="displayComponents"
       :config="config"
-      editable
     />
   </div>
 </template>
@@ -49,6 +49,11 @@ export default {
   src: url("~@/assets/HomemadeApple.ttf");
 }
 
+@font-face {
+  font-family: "Lilita One";
+  src: url('~@/assets/LilitaOne-Regular.ttf');
+}
+
 #app {
   font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -56,6 +61,13 @@ export default {
   color: #2c3e50;
   width: 900px;
   margin: 0 auto;
+}
+
+.site-title {
+  font-family: 'Lilita One',cursive;
+  font-size: 50px;
+  color: #8fcff4;
+  text-align: center;
 }
 
 @media only screen and (max-width: 600px) {
